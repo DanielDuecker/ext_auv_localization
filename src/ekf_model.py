corrected faulty depth information of the cameras by scaling the z dimension with a factor of 1/1.35

src/ekf_model.py
@@ -27,7 +27,7 @@ class EkfLocalization:
 		self._H_cam_at_x = 0
 		self._R_mat = 0
 		self._F = np.eye(6)
-		self._time_stamp = 0
+		#self._time_stamp = 0.0
 		self._cam_id = 0
 		# testing: publishing z_tilde
 		self._pub = rospy.Publisher('/z_tilde', callback_data, queue_size = 1)
@@ -55,10 +55,24 @@ class EkfLocalization:
 		z_tilde_msg.h = z_tilde
 		self._pub.publish(z_tilde_msg)
 		# testing end
+		# limit size of angles by +/- pi
 		self._x_hat = self._x_hat + K.dot(z_tilde)
 		self._P_mat = (np.eye(6) - K.dot(H_cam_at_x)).dot(self._P_mat)
-		print 'z_tilde:'
-		print z_tilde
+		if self._x_hat[3] > 0.0:
+			self._x_hat[3] = np.mod(self._x_hat[3], np.pi)
+		elif self._x_hat[3] < 0.0:
+			self._x_hat[3] = -np.mod(-self._x_hat[3], np.pi)
+		
+		if self._x_hat[4] > 0.0:
+			self._x_hat[4] = np.mod(self._x_hat[4], np.pi)
+		elif self._x_hat[4] < 0.0:
+			self._x_hat[4] = -np.mod(-self._x_hat[4], np.pi)
+			
+		if self._x_hat[5] > 0.0:
+			self._x_hat[5] = np.mod(self._x_hat[5], np.pi)
+		elif self._x_hat[5] < 0.0:
+			self._x_hat[5] = -np.mod(-self._x_hat[5], np.pi)
+		
 
 		"""
 		L = auxiliary variable
@@ -87,7 +101,7 @@ class EkfLocalization:
 
 		# publishing Position, Orientation and Covariance as output of the Extended-Kalman-Filter
 		ekfOutput_msg = PoseWithCovarianceStamped()
-		ekfOutput_msg.header.stamp = self._time_stamp
+		ekfOutput_msg.header.stamp = time_stamp
 		ekfOutput_msg.header.frame_id = 'cube'
 		ekfOutput_msg.pose.pose.position.x = x_hat[0]
 		ekfOutput_msg.pose.pose.position.y = x_hat[1]
@@ -97,14 +111,15 @@ class EkfLocalization:
 		ekfOutput_msg.pose.pose.orientation.z = quat[2]
 		ekfOutput_msg.pose.pose.orientation.w = quat[3]
 		covar = P.flatten()  # published covariance matrix must be of type 'array' while P is still of type 'numpy.ndarray'. flatten() converts type ndarray to type array
-		ekfOutput_msg.pose.covariance = covar
+		
+		ekfOutput_msg.pose.covariance = covar.tolist()
 		self._pub_ekf_output.publish(ekfOutput_msg)
 
 
 		# publishing the tf-transformation of the cube/boat/object-frame
 		transforms = []
 		ekf_tf_msg = TransformStamped()
-		ekf_tf_msg.header.stamp = time_stamp
+		ekf_tf_msg.header.stamp  = time_stamp
 		ekf_tf_msg.header.frame_id = 'map'
 		ekf_tf_msg.child_frame_id = 'cube'
 		# changing world frame to NWU which is rviz standart coordinate frame convention (doing so by converting quat into a rotation matrix and transforming it into rviz standart)

src/ekf_node.py
@@ -18,6 +18,9 @@ from geometry_msgs.msg import PoseWithCovarianceStamped
 from geometry_msgs.msg import TransformStamped
 from ekf.msg import callback_data
 from pyquaternion import Quaternion  # defined q = (w, x, y, z)
+#testing
+from std_msgs.msg import String
+from sensor_msgs.msg import CompressedImage
 
 
 def callback1(msg):  # the purpose of this function is to recieve the position data of all seen tags from camera 1, create the h, H and R Matrices according to the seen tags and give them to ekf for further processing while publishing the tf-frame of it's camera
@@ -31,18 +34,45 @@ def callback2(msg):  # sole purpose of this function is to recieve the position 
 	global testcam2, ekf, pub_tf_cam2, no_tag2
 	
 	testcam2.callcam(ekf, pub_tf_cam2, no_tag2, msg)
-	
 
+def callback3(msg):
+
+	global testcam3, ekf, pub_tf_cam3, no_tag3
+
+	testcam3.callcam(ekf, pub_tf_cam3, no_tag3, msg)
+
+def callback4(msg):
+
+	global testcam4, ekf, pub_tf_cam4, no_tag4
+	
+	testcam4.callcam(ekf, pub_tf_cam4, no_tag4, msg)
+	
+def callback5(msg):
+
+	global testcam5, ekf, pub_tf_cam5, no_tag5
+	
+	testcam5.callcam(ekf, pub_tf_cam5, no_tag5, msg)
+	
+def callback6(msg):
+
+	global testcam6, ekf, pub_tf_cam6, no_tag6
+	
+	testcam6.callcam(ekf, pub_tf_cam6, no_tag6, msg)
+	
 
 # initiating publishers
 pub_ekf_output = rospy.Publisher('/ekf_position', PoseWithCovarianceStamped, queue_size = 1)
 pub_tf_object = tf2_ros.TransformBroadcaster()
 pub_tf_cam1 = tf2_ros.TransformBroadcaster()
 pub_tf_cam2 = tf2_ros.TransformBroadcaster()
+pub_tf_cam3 = tf2_ros.TransformBroadcaster()
+pub_tf_cam4 = tf2_ros.TransformBroadcaster()
+pub_tf_cam5 = tf2_ros.TransformBroadcaster()
+pub_tf_cam6 = tf2_ros.TransformBroadcaster()
 
 # initiating globals for all cameras
-x_hat_0 = np.array([0, 0, 0, 0, 0, 0]).reshape(6, 1)
-skalar = 1000000
+x_hat_0 = np.array([0.25, 0.45, -0.8, 0, 0, 0]).reshape(6, 1)
+skalar = 10000
 P_mat_0 = np.diag([10*skalar, 10*skalar, 10*skalar, 0.5, 0.5, 0.5])
 process_noise_pos = 0.05
 process_noise_angle = 0.1
@@ -60,7 +90,8 @@ R_cam1 = np.diag(np.array([1.5 * (10 ** -6), 1.5 * (10 ** -6), 3 * (10 ** -4),
 			   1.5 * (10 ** -6), 1.5 * (10 ** -6), 3 * (10 ** -4),
 			   1.5 * (10 ** -6), 1.5 * (10 ** -6), 3 * (10 ** -4),
 			   1.5 * (10 ** -6), 1.5 * (10 ** -6), 3 * (10 ** -4)]))*1000 # measurement noise
-testcam1 = cam.camera(1, -0.32, 1.525, -0.895, 0, 0, 0, R_cam1)
+testcam1 = cam.camera(1, 0.062, 1.59, -0.76, 0, 0, 0, R_cam1) # new pos: x = 0.062, y = 1.59, z = -0.76
+# old position = -0.32, 1.525, -0.895
 no_tag1 = 1 # init "no tag" counter
 
 # initiating camera 2
@@ -69,13 +100,55 @@ R_cam2 = np.diag(np.array([1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4),
 			   1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4),
 			   1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4),
 			   1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4)]))*1000 # measurement noise
-testcam2 = cam.camera(2, -0.32, 0.326, -0.895, 0, 0, 0, R_cam2)
+testcam2 = cam.camera(2, 0.062, 0.41, -0.76, 0, 0, 0, R_cam2) # new pos: x = 0.062, y = 0.41, z = -0.76
+# old position = -0.32, 0.326, -0.895
 no_tag2 = 1 # init "no tag" counter
+
+#initiating camera 3
+R_cam3 = np.diag(np.array([1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4),
+			   1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4),
+			   1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4),
+			   1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4),
+			   1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4)]))*10000 # measurement noise
+testcam3 = cam.camera(3, 4.205, 0.12, -0.57, 0, 0, 3*math.pi/4, R_cam3)
+no_tag3 = 1 # init "no tag" counter
+
+#initiating camera 4
+R_cam4 = np.diag(np.array([1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4),
+			   1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4),
+			   1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4),
+			   1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4),
+			   1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4)]))*10000 # measurement noise
+testcam4 = cam.camera(4, 0.11, 1.965, -0.57, 0, 0, -math.pi/4, R_cam4) # position might be a bit off, it has been measurured in relation to cam 1\\ new pos: x = 0.11, y = 1.965, z = -0.57
+# old position = -0.26, 1.9, -0.71
+no_tag4 = 1 # init "no tag" counter
+
+#initiating camera 5
+R_cam5 = np.diag(np.array([1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4),
+			   1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4),
+			   1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4),
+			   1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4),
+			   1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4)]))*1000 # measurement noise
+testcam5 = cam.camera(5, 4.160, 1.595, -0.795, 0, 0, math.pi, R_cam5) # positions not right yet
+no_tag5 = 1 # init "no tag" counter
+
+#initiating camera 6
+R_cam6 = np.diag(np.array([1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4),
+			   1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4),
+			   1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4),
+			   1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4),
+			   1.5 * (10 ** -6), 1.5 * (10 ** -6), 1.5 * (10 ** -4)]))*1000 # measurement noise
+testcam6 = cam.camera(6, 4.16, 0.405, -0.76, 0, 0, math.pi, R_cam6)
+no_tag6 = 1 # init "no tag" counter
 
 def main():
 	rospy.init_node('ext_auv_localization')
 	rospy.Subscriber("/tag_detections1", AprilTagDetectionArray, callback1, queue_size=1)
 	rospy.Subscriber("/tag_detections2", AprilTagDetectionArray, callback2, queue_size=1)
+	rospy.Subscriber("/tag_detections3", AprilTagDetectionArray, callback3, queue_size=1)
+	rospy.Subscriber("/tag_detections4", AprilTagDetectionArray, callback4, queue_size=1)
+	rospy.Subscriber("/tag_detections5", AprilTagDetectionArray, callback5, queue_size=1)
+	rospy.Subscriber("/tag_detections6", AprilTagDetectionArray, callback6, queue_size=1)
 	rospy.spin()
 
 